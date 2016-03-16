abba96fdc37e10865cd7f044855290e1d30fd1d3
M1kEk1M <M1kEK1M1993@gmail.com>
2016-03-16 09:59:36 +0300

    skill class

---
+++ game/scrypts/skills.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+
+class Skill(object):
+    def __init__(self, name, attribute='spirit'):
+        self.name = name
+        self.attribute = attribute
+        self.training = False
+        self.expirience = False
+        self.specialisation = False
+        self.talent = False
+
+
+    def level(self):
+        level = 0
+        if self.training:
+            level += 1
+        if self.expirience:
+            level += 1
+        if self.specialisation:
+            level += 1
+        if self.talent:
+            level += 1
+        return level
