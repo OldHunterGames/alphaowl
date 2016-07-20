d3f8b6444c26f40996cb6554fc7df6f3a3be0858
M1kEk1M <M1kEK1M1993@gmail.com>
2016-07-20 21:54:25 +0300

    hack

--- game/scrypts/obj_character.py
+++ game/scrypts/obj_character.py
@@ -15,7 +15,8 @@ from schedule import *
 from taboos import init_taboos
 from relations import Relations
 from stance import Stance
-
+class NoneNameObject(object):
+    name = None
 
 accommodation_types = {'makeshift bad': {'comfort': -3},
                        'campfire': {'comfort': -1},
@@ -751,13 +752,13 @@ class Person(object):
         for f in self.features:
             if f.slot == slot:
                 return f
-        return None
+        return NoneNameObject
 
     def feature(self, name):                # finds feature with needed name if exist
         for f in self.features:
             if f.name == name:
                 return f
-        return None
+        return NoneNameObject
 
     def remove_feature(self, feature):       # feature='str' or Fearutere()
         if isinstance(feature, str):
